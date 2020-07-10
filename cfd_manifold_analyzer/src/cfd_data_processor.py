86edf7ca6bf8729b7c7971ac3edd035700b008cd
lufire <lukas.feierabend@gmail.com>
2020-07-10 19:53:44 +0200

    implementing class structure

--- cfd_manifold_analyzer/src/cfd_data_processor.py
+++ cfd_manifold_analyzer/src/cfd_data_processor.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from scipy.interpolate import griddata
 import os
 import matplotlib.pyplot as plt
@@ -6,6 +7,7 @@ import matplotlib
 import timeit
 from ..settings import file_names
 from ..settings import geometry as geom
+from . import constants
 
 matplotlib.use('TkAgg')
 
@@ -39,6 +41,7 @@ class CFDDataChannel:
         self.x = np.zeros(self.nx)
         self.coords = np.zeros((self.dims, self.nx))
         self.cord_length = np.linspace(0.0, self.length, self.nx)
+        self.inlet_mass_flow = 0.0
 
     def create_coords(self):
         length_vector = self.length * self.direction_vector
@@ -50,7 +53,10 @@ class CFDDataChannel:
         self.coords[:] = coords
         return coords
 
-    def plot(self, xaxis=None, yaxis='pressure', ax=None):
+    def plot(self, xaxis=None, yaxis='pressure',
+             xlabel='No xlabel argument given',
+             ylabel='No ylabel argument supplied',
+             ax=None):
         if ax is None:
             fig, ax = plt.subplots()
         if xaxis is None:
@@ -58,12 +64,51 @@ class CFDDataChannel:
         else:
             raise ValueError('data not found')
         ax.plot(xaxis, self.data[yaxis])
+        ax.set_xlabel('Channels [-]')
+        ax.set_ylabel('Normalized Mass Flows [-]')
         plt.show()
 
 
-class CFDManifoldProcessor3D:
+class CFDMassFlowProcessor:
     def __init__(self, file_path, output_dir):
         self.file_path = file_path
+        self.output_dir = output_dir
+        self.mass_flow_name = file_names.mass_flow_name
+        self.total_mass_flow_name = file_names.total_mass_flow_name
+        self.n_channels = None
+        self.mass_flows = None
+        self.total_mass_flow = 0.0
+
+    def load_2d_data(self):
+        return pd.read_csv(os.path.join(self.file_path),
+                           sep='\t', header=[0, 1])
+
+    def get_mass_flows(self):
+        cfd_data_2d = self.load_2d_data()
+        self.mass_flows = \
+            cfd_data_2d[self.mass_flow_name].iloc[-2].to_numpy()
+        self.n_channels = range(len(self.mass_flows))
+        self.total_mass_flow = \
+            cfd_data_2d[self.total_mass_flow_name].iloc[-2][0]
+        return self.mass_flows
+
+    def plot(self, ax=None):
+        if ax is None:
+            fig, ax = plt.subplots()
+        mean_mass_flow = self.mass_flows.mean()
+        ax.plot(self.n_channels, self.mass_flows / mean_mass_flow)
+        ax.set_xlabel('Channels [-]')
+        ax.set_ylabel('Normalized Mass Flows [-]')
+        plt.tight_layout()
+        plt.show()
+        np.save(os.path.join(full_output_dir, path.mass_flow_data_file),
+                channel_mass_flows)
+
+
+class CFDManifoldProcessor3D:
+    def __init__(self, file_path_3d, file_path_2d, output_dir):
+        self.file_path_3d = file_path_3d
+        self.file_path_2d = file_path_2d
         self.output_dir = output_dir
         # create output folder
         if not os.path.isdir(output_dir):
@@ -97,13 +142,17 @@ class CFDManifoldProcessor3D:
         self.n_channels = len(self.channels)
         self.n_manifolds = len(self.manifolds)
 
-    def load_data(self):
-        if not self.file_path.split('.')[-1] == 'npy':
-            raise IOError('file must be numpy binary file with .npy-extension')
-        return np.load(self.file_path).transpose()
+    def load_3d_data(self):
+        file_ext = self.file_path_3d.split('.')[-1]
+        if file_ext == 'npy':
+            return np.load(self.file_path_3d).transpose()
+        elif file_ext == 'dat':
+            return np.loadtxt(self.file_path_3d).transpose()
+        else:
+            raise IOError('file must be ascii (.dat) or binary (.npy)')
 
     def data_to_array(self):
-        raw_data = self.load_data()
+        raw_data = self.load_3d_data()
         # create coordinate and pressure arrays corresponding to coordinate system
         # configuration in AVL FIRE case setup
         x, y, z, p = [], [], [], []
@@ -153,7 +202,7 @@ class CFDManifoldProcessor3D:
         data_coords_transposed = data_coords.transpose()
         non_zero_axis = []
         for i in range(len(data_coords_transposed)):
-            if np.var(data_coords_transposed[i]) > 1e-8:
+            if np.var(data_coords_transposed[i]) > constants.SMALL:
                 non_zero_axis.append(i)
         stripped_data_coords = np.take(data_coords, non_zero_axis, axis=-1)
         stripped_coords_combined = \
