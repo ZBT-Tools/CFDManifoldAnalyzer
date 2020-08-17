3967d97a6b0ad7f20ddb0f81cf8c0a954f54fcd3
lufire <lukas.feierabend@gmail.com>
2020-08-17 19:00:49 +0200

    zeta calculation, extending into classes

--- cfd_manifold_analyzer/run_t-junction_processor.py
+++ cfd_manifold_analyzer/run_t-junction_processor.py
@@ -49,20 +49,24 @@ mass_flows = cfd_data.mass_flow_data.mass_flows
 
 # get channel outlet pressure
 x_out_channel = geom.channel_start_vector[0][1] + channel.length
-p_channel_out = cfd_data.channels[0].data_function['pressure'](x_out_channel)
-print(p_channel_out)
-p_manifold_out = \
+p_chl_out = cfd_data.channels[0].data_function['pressure'](x_out_channel)
+print(p_chl_out)
+p_mfd_out = \
     cfd_data.manifolds[0].data_function['pressure'](geom.manifold_range[-1])
-print(p_manifold_out)
+print(p_mfd_out)
 
+p_mfd_in = \
+    cfd_data.manifolds[0].data_function['pressure'](geom.manifold_range[0])
+p_mfd = [p_mfd_in, p_mfd_out]
+p_chl = [None, p_chl_out]
 
 # channel update
-channel.p_out = p_channel_out
+channel.p_out = p_chl_out
 channel.update(mass_flow_in=channel_mass_flow,
                update_heat=False)
 
 # manifold update
-manifold.p_out = p_manifold_out
+manifold.p_out = p_mfd_out
 mass_source = - channel_mass_flow
 manifold.update(mass_flow_in=manifold_mass_flow,
                 mass_source=mass_source, update_heat=False)
@@ -70,4 +74,54 @@ manifold.update(mass_flow_in=manifold_mass_flow,
 print(manifold.velocity)
 print(manifold.reynolds)
 print(channel.velocity)
+mfd = manifold
+chl = channel
+# set length segments according to analysation points
+dx_mfd = [0.0 - geom.manifold_range[0], geom.manifold_range[1] - 0.0]
+dx_chl = geom.channel_length
+
+
+# get tube friction factors
+for zeta in manifold.zetas:
+    if isinstance(zeta, pemfc.flow_resistance.WallFrictionFlowResistance):
+        f_mfd = zeta.value * manifold.d_h / (manifold.dx_node * 0.5)
+
+for zeta in channel.zetas:
+    if isinstance(zeta, pemfc.flow_resistance.WallFrictionFlowResistance):
+        f_chl = np.average(zeta.value * channel.d_h / (channel.dx_node * 0.5))
+
+# calculate t-junction flow resistance factor within manifold
+v1_v0_2 = (mfd.velocity[1] / mfd.velocity[0]) ** 2.0
+rho1_rho0 = mfd.fluid.density[1] / mfd.fluid.density[0]
+
+zeta_mfd = 1.0 \
+    + (p_mfd[0] - p_mfd[1]) \
+    * 2.0 / (mfd.fluid.density[0] * mfd.velocity[0] ** 2.0) \
+    - rho1_rho0 * v1_v0_2 \
+    - dx_mfd[0] / mfd.d_h * f_mfd[0] \
+    - rho1_rho0 * v1_v0_2 * dx_mfd[1] / mfd.d_h * f_mfd[1]
+
+# calculate t-junction flow resistance factor from manifold to channel
+v2_v0_2 = (chl.velocity[1] / mfd.velocity[0]) ** 2.0
+rho2_rho0 = chl.fluid.density[1] / mfd.fluid.density[0]
+zeta_chl = 1.0 \
+    + (p_mfd[0] - p_chl[1]) \
+    * 2.0 / (mfd.fluid.density[0] * mfd.velocity[0] ** 2.0) \
+    - rho2_rho0 * v2_v0_2 \
+    - dx_mfd[0] / mfd.d_h * f_mfd[0] \
+    - rho2_rho0 * v2_v0_2 * dx_chl / chl.d_h * f_chl
+
+print(zeta_mfd)
+print(zeta_chl)
+
+# calculate t-junction flow resistance factor
+# including dynamic and tube resistance
+zeta_mfd_2 = (p_mfd[1] - p_mfd[0]) \
+    * 2.0 / (mfd.fluid.density[0] * mfd.velocity[0] ** 2.0)
+zeta_chl_2 = (p_chl[1] - p_mfd[0]) \
+    * 2.0 / (mfd.fluid.density[0] * mfd.velocity[0] ** 2.0)
+
+print(zeta_mfd_2)
+print(zeta_chl_2)
+
 
