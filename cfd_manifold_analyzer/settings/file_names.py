38d0612d3dada2aad73fa2d586a054f1a921f933
lufire <lukas.feierabend@gmail.com>
2020-08-21 10:03:29 +0200

    multiple case processing implemented

--- cfd_manifold_analyzer/settings/file_names.py
+++ cfd_manifold_analyzer/settings/file_names.py
@@ -6,7 +6,7 @@ Paths to AVL FIRE output data: directory, 3D cut file, 2D results file
 """
 
 # AVL FIRE results directory
-dir_name = r'C:\Users\feierabend\PycharmProjects\CFDManifoldAnalyzer\data'
+dir_name = r'C:\Users\lukas\PycharmProjects\CFDManifoldAnalyzer\data'
 
 # 3D cut file name
 # Each row of file has three vertices with their respective pressure values:
